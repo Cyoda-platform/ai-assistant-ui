f03359010242e4fc15a89c9cdf29ac88eadbeae1
alexander.savenko <alexander.savenko@cyoda.com>
2024-12-23 16:48:13 +0300

    Prepare for canvas

---
+++ src/components/ChatBot/ChatBotCanvas.vue
@@ -0,0 +1,14 @@
+<template>
+  <Editor class="chat-bot-canvas"/>
+</template>
+
+<script setup lang="ts">
+
+import Editor from "@/components/Editor/Editor.vue";
+</script>
+
+<style lang="scss">
+.chat-bot-canvas {
+  min-height: 100%;
+}
+</style>
---
+++ src/components/Editor/Editor.vue
@@ -0,0 +1,110 @@
+<template>
+  <div ref="rootRef" class="editor" :class="{
+    'editor-disable': !editable
+  }" style="height: 400px; width: 100%;"></div>
+</template>
+
+<script setup lang="ts">
+import {ref, computed, watch, onMounted, onBeforeUnmount, nextTick} from "vue";
+import * as monaco from 'monaco-editor';
+
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    return new editorWorker()
+  }
+}
+
+const emit = defineEmits(["update:modelValue", "ready"]);
+const rootRef = ref(null);
+const props = defineProps({
+  modelValue: {default: ""},
+  language: {default: "plain"},
+  editable: {default: true},
+});
+
+const language = computed(() => {
+  return 'text/plain'
+})
+
+let editor: any = null;
+
+onMounted(() => {
+  editor = monaco.editor.create(rootRef.value, {
+    value: props.modelValue,
+    language: language.value,
+    automaticLayout: true,
+    readOnly: !props.editable,
+    renderLineHighlight: "none",
+    overviewRulerBorder: false,
+    minimap: {enabled: false},
+    theme: 'vs-dark',
+    padding: {
+      top: 10
+    }
+  });
+
+  editor.getModel().onDidChangeContent((e) => {
+    updateListenerExtension(editor);
+  })
+
+  nextTick(() => {
+    emit('ready');
+  })
+  return;
+
+});
+
+function updateListenerExtension(editor) {
+  let value = editor.getValue();
+  emit("update:modelValue", value);
+}
+
+function isJsonValid(str: string) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+watch(
+  () => props.modelValue,
+  ([modelValue, newString]) => {
+    let newValue = modelValue || newString;
+    if (editor) {
+      const editorValue = editor.getValue();
+      if (newValue !== editorValue) {
+        editor.setValue(newValue);
+      }
+    }
+  },
+  {immediate: true}
+);
+
+watch(language, () => {
+  if (!editor) return;
+  monaco.editor.setModelLanguage(editor.getModel(), language.value)
+})
+</script>
+
+<style lang="scss">
+.editor {
+  border: 1px solid silver;
+  outline: none;
+
+  .lines-content.monaco-editor-background {
+    padding-left: 5px !important;
+  }
+
+  &.editor-disable .monaco-editor-background, &.editor-disable .minimap {
+    background-color: #F5F7FA !important;
+  }
+
+  .scroll-decoration {
+    display: none;
+  }
+}
+</style>
