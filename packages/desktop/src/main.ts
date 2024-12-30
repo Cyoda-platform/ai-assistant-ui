d5cc7d6a73b7bd8599e4a9bd5c1362e8a3a5ccaf
alexander.savenko <alexander.savenko@cyoda.com>
2024-12-30 23:41:08 +0300

    Desktop

--- packages/desktop/src/main.ts
+++ packages/desktop/src/main.ts
@@ -1,32 +1,35 @@
-import { app, BrowserWindow, screen } from 'electron';
+import {app, BrowserWindow, screen, Notification} from 'electron';
 import path from 'path';
 import started from 'electron-squirrel-startup';
+import {updateElectronApp} from 'update-electron-app';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width,
-    height,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width,
+        height,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+    // and load the index.html of the app.
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    } else {
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools();
+    }
 };
 
 // This method will be called when Electron has finished
@@ -38,18 +41,20 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+updateElectronApp(); // additional configuration options available
--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import useAuthStore from "@/stores/auth.ts";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: process.env.IS_ELECTRON ? createWebHashHistory() : createWebHistory(),
+  history: import.meta.env.VITE_IS_ELECTRON ? createWebHashHistory() : createWebHistory(),
   routes: [
     {
       path: "/",
