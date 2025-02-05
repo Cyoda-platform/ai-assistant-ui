d6e4b1a82bc48d177608353b8cfd852d21d95a09
alexander.savenko <alexander.savenko@cyoda.com>
2025-02-06 01:30:22 +0300

    feat: 44 | Add Markdown for the canvas mode

--- packages/web/src/components/ChatBot/ChatBotMessageQuestion.vue
+++ packages/web/src/components/ChatBot/ChatBotMessageQuestion.vue
@@ -5,19 +5,63 @@
           <AiChaIcon/>
         </span>
       <span>CYODA AI</span>
+
+      <div class="chat-bot-message-question__top-actions">
+        <el-tooltip
+          class="box-item"
+          effect="dark"
+          content="Rollback"
+          show-after="1000"
+          placement="top"
+        >
+          <el-button
+            @click="onClickRollbackQuestion"
+            :loading="isLoadingRollback"
+            size="small"
+            class="btn-default btn-icon"
+          >
+            <RollbackQuestionIcon/>
+          </el-button>
+        </el-tooltip>
+      </div>
+
     </div>
     <div v-html="computedMessage" class="chat-bot-message-question__body"></div>
-    <div class="chat-bot-message-question__actions">
-      <el-button
-        @click="onClickRollbackQuestion"
-        :loading="isLoading"
-        class="btn-default chat-bot-message-question__action-rollback"
-        :class="{
-          'is-loading': isLoading
-        }"
+    <div class="chat-bot-message-question__bottom-actions">
+
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Copy"
+        placement="top"
+        show-after="1000"
       >
-        <RollbackQuestionIcon class="chat-bot-message-question__rollback-icon"/>
-      </el-button>
+        <el-button
+          @click="onClickCopy"
+          size="small"
+          class="btn-default btn-icon"
+        >
+          <CopyIcon/>
+        </el-button>
+      </el-tooltip>
+
+      <el-tooltip
+        v-if="message.approve"
+        class="box-item"
+        effect="dark"
+        content="Approve"
+        placement="top"
+        show-after="1000"
+      >
+        <el-button
+          @click="onClickApproveQuestion"
+          size="small"
+          class="btn-default btn-icon"
+          :loading="isLoadingApprove"
+        >
+          <ThumbUpIcon/>
+        </el-button>
+      </el-tooltip>
     </div>
   </div>
 </template>
@@ -25,15 +69,20 @@
 <script lang="ts" setup>
 import AiChaIcon from "@/assets/images/icons/ai-chat.svg";
 import RollbackQuestionIcon from "@/assets/images/icons/rollback-question.svg";
+import CopyIcon from "@/assets/images/icons/copy.svg";
+import ThumbUpIcon from "@/assets/images/icons/thumb-up.svg";
 import {computed, ref} from "vue";
 import HelperMarkdown from "@/helpers/HelperMarkdown";
+import HelperCopy from "@/helpers/HelperCopy";
+import {ElNotification} from "element-plus";
 
 const props = defineProps<{
   message: any,
 }>()
 
-const emit = defineEmits(['rollbackQuestion']);
-const isLoading = ref(false);
+const emit = defineEmits(['rollbackQuestion', 'approveQuestion']);
+const isLoadingRollback = ref(false);
+const isLoadingApprove = ref(false);
 const computedMessage = computed(() => {
   const text = props.message.text;
   if (typeof text === 'object' && text !== null) {
@@ -44,8 +93,22 @@ const computedMessage = computed(() => {
 });
 
 function onClickRollbackQuestion() {
-  isLoading.value = true;
+  isLoadingRollback.value = true;
   emit('rollbackQuestion', props.message.raw);
+}
+
+function onClickApproveQuestion() {
+  isLoadingApprove.value = true;
+  emit('approveQuestion', props.message.raw);
+}
+
+function onClickCopy() {
+  HelperCopy.copy(props.message.text);
+  ElNotification({
+    title: 'Success',
+    message: 'The data has been copied',
+    type: 'success',
+  })
 }
 </script>
 
@@ -81,31 +144,30 @@ function onClickRollbackQuestion() {
     }
   }
 
+  &__top-actions {
+    margin-left: auto;
+    position: relative;
+    top: -10px;
+  }
+
+  &__bottom-actions {
+    text-align: right;
+  }
+
   &__body {
     font-size: 16px;
     line-height: 1.5;
     padding-right: 48px;
     padding-bottom: 16px;
     word-break: break-all;
-    ul, ol{
+
+    ul, ol {
       padding: 0 0 0 20px;
     }
   }
 
   &__actions {
     text-align: right;
-  }
-
-  &__action-rollback {
-    width: 28px;
-    height: 28px;
-    padding: 0;
-
-    &.is-loading {
-      span {
-        display: none;
-      }
-    }
   }
 }
 </style>
--- packages/web/src/helpers/markdown/actions.ts
+++ packages/web/src/helpers/markdown/actions.ts
@@ -1,3 +1,5 @@
+import HelperCopy from "../HelperCopy";
+
 export default function markdownActions(element: HTMLElement, raw) {
   const copyButton = element.querySelector('.copy') as HTMLAnchorElement | null;
   const copySpan = copyButton?.querySelector('span') as HTMLSpanElement | null;
@@ -6,15 +8,12 @@ export default function markdownActions(element: HTMLElement, raw) {
     copyButton.addEventListener("click", (e) => {
       e.preventDefault();
 
-      if (navigator.clipboard) {
-        navigator.clipboard.writeText(raw)
-          .then(() => {
+        HelperCopy.copy(raw).then(() => {
             copySpan.textContent = 'copied!';
             setTimeout(() => {
               copySpan.textContent = 'copy';
             }, 2000);
           })
-      }
     });
   }
 }
