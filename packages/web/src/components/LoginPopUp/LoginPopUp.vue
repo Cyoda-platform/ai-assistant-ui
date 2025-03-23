a51d5cdf95302340946e042eaec9e60ba358be87
alexander.savenko <alexander.savenko@cyoda.com>
2025-03-24 00:26:24 +0300

    feat: 60 | remove login screen

---
+++ packages/web/src/components/LoginPopUp/LoginPopUp.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dialog
+    v-model="dialogVisible"
+    title="You are out of credits"
+    width="400"
+  >
+    <span>Log in to continue building beautifully for free.</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible=false">
+          Cancel
+        </el-button>
+        <el-button @click="loginWithRedirect" class="btn btn-primary" type="primary">
+          Login
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import {onBeforeUnmount, onMounted, ref} from "vue";
+import eventBus from "@/plugins/eventBus";
+import {SHOW_LOGIN_POPUP} from "@/helpers/HelperConstants";
+import {useAuth0} from "@auth0/auth0-vue";
+
+const {loginWithRedirect} = useAuth0();
+
+const dialogVisible = ref(false);
+
+onMounted(() => {
+  eventBus.$on(SHOW_LOGIN_POPUP, showLogin)
+})
+
+onBeforeUnmount(() => {
+  eventBus.$off(SHOW_LOGIN_POPUP, showLogin);
+})
+
+function showLogin() {
+  dialogVisible.value = true;
+}
+</script>
+
+<style scoped lang="scss">
+.dialog-footer {
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+    flex: 1;
+  }
+}
+</style>
--- packages/web/src/helpers/HelperConstants.ts
+++ packages/web/src/helpers/HelperConstants.ts
@@ -1,1 +1,3 @@
 export const UPDATE_CHAT_LIST = 'chat-list:update';
+export const SHOW_LOGIN_POPUP = 'showLoginPopUp';
+export const LOGIN_REDIRECT_URL = 'loginRedirectUrl';
