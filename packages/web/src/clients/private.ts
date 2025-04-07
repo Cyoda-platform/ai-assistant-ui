a0a5e64201e9e4866e5e1d708e2ed7c62e58a2cb
alexander.savenko <alexander.savenko@cyoda.com>
2025-04-08 00:01:34 +0300

    Axios Retry

--- packages/web/src/clients/private.ts
+++ packages/web/src/clients/private.ts
@@ -2,12 +2,18 @@ import axios from "axios";
 import errorInterceptor from "@/clients/interceptors/error.ts";
 import jwtInterceptor from "@/clients/interceptors/jwt.ts";
 import refreshToken from "@/clients/interceptors/refreshToken.ts";
+import axiosRetry from 'axios-retry';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_BASE,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
+});
+
+axiosRetry(instance, {
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
 });
 
 errorInterceptor(instance);
