5889dfe003377810af8cb1f4bd057395691d8c3e
alexander.savenko <alexander.savenko@cyoda.com>
2025-04-29 00:03:29 +0300

    Handle 403 errors for `/v1/transfer-chats` in Axios interceptor

--- packages/web/src/clients/interceptors/error.ts
+++ packages/web/src/clients/interceptors/error.ts
@@ -1,5 +1,5 @@
 import type {AxiosResponse, AxiosError, AxiosInstance} from "axios";
-import HelperErrors from "@/helpers/HelperErrors.ts";
+import HelperErrors from "../../helpers/HelperErrors";
 import eventBus from "../../plugins/eventBus";
 import {SHOW_LOGIN_POPUP} from "../../helpers/HelperConstants";
 
@@ -8,6 +8,10 @@ const errorInterceptor = (instance: AxiosInstance): void => {
     (response: AxiosResponse) => response,
     (error: AxiosError) => {
       const response = error.response;
+      if(response?.config?.url.includes('/v1/transfer-chats') && [403].includes(response?.status)) {
+        HelperErrors.handler(error);
+        return Promise.reject(error);
+      }
       if ([403, 429].includes(response?.status)) {
         eventBus.$emit(SHOW_LOGIN_POPUP);
         return Promise.reject(error);
