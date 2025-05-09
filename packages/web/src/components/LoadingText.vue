7bbd21e3003b83443f35f338e74c2fc257e13048
alexander.savenko <alexander.savenko@cyoda.com>
2025-05-09 23:21:38 +0300

    feat: 79 | enable dynamic UI labels retrival

--- packages/web/src/components/LoadingText.vue
+++ packages/web/src/components/LoadingText.vue
@@ -4,12 +4,14 @@
 
 <script setup lang="ts">
 import {computed, onBeforeUnmount, onMounted, ref} from "vue";
+import {useI18n} from "vue-i18n";
+const { t } = useI18n();
 
 const counter = ref(0);
 let timeIntervalId = null;
 
 const label = computed(() => {
-  return `Loading ${'.'.repeat(counter.value)}`
+  return `${t('common.loading')} ${'.'.repeat(counter.value)}`
 });
 
 onMounted(init);
