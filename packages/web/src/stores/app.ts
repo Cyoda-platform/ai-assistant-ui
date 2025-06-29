b341a95d84154741a20a01b3678fd79c3a580c0e
alexander.savenko <alexander.savenko@cyoda.com>
2025-06-30 00:57:39 +0300

    feat: 102 | Adjust AI assistant UI to show terms & conditions, privacy policy, and link to Cyoda home

--- packages/web/src/stores/app.ts
+++ packages/web/src/stores/app.ts
@@ -4,27 +4,32 @@ import HelperStorage from "@/helpers/HelperStorage.ts";
 const helperStorage = new HelperStorage();
 
 const useAppStore = defineStore('app', {
-  state: () => {
-    return {
-      isSidebarHidden: helperStorage.get('app:isSidebarHidden', false),
-      isCanvasHidden: helperStorage.get('app:isCanvasHidden', false),
-      theme: helperStorage.get('app:theme', 'system'),
-    }
-  },
-  actions: {
-    toggleSidebar(value) {
-      this.isSidebarHidden = value !== undefined ? value : !this.isSidebarHidden;
-      helperStorage.set("app:isSidebarHidden", this.isSidebarHidden);
+    state: () => {
+        return {
+            isSidebarHidden: helperStorage.get('app:isSidebarHidden', false),
+            isCanvasHidden: helperStorage.get('app:isCanvasHidden', false),
+            theme: helperStorage.get('app:theme', 'system'),
+            consentDialog: helperStorage.get('app:consentDialog', true),
+        }
     },
-    toggleCanvas() {
-      this.isCanvasHidden = !this.isCanvasHidden;
-      helperStorage.set("app:isCanvasHidden", this.isCanvasHidden);
+    actions: {
+        toggleSidebar(value) {
+            this.isSidebarHidden = value !== undefined ? value : !this.isSidebarHidden;
+            helperStorage.set("app:isSidebarHidden", this.isSidebarHidden);
+        },
+        toggleCanvas() {
+            this.isCanvasHidden = !this.isCanvasHidden;
+            helperStorage.set("app:isCanvasHidden", this.isCanvasHidden);
+        },
+        setTheme(theme) {
+            this.theme = theme;
+            helperStorage.set("app:theme", theme);
+        },
+        setConsentDialog(value) {
+            this.consentDialog = value;
+            helperStorage.set("app:consentDialog", value);
+        }
     },
-    setTheme(theme) {
-      this.theme = theme;
-      helperStorage.set("app:theme", theme);
-    }
-  },
 });
 
 export default useAppStore;
