d947e0ce18d852df83e8224464737e37d205e891
alexander.savenko <alexander.savenko@cyoda.com>
2025-07-04 00:47:04 +0300

    feat: 99 | workflow visualisation

---
+++ packages/web/src/components/ChatBot/ChatBotEditorWorkflow/Node.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="node">
+    <strong>{{ data.label }}</strong>
+
+    <div v-if="data.editableCondition">
+                <textarea
+                    :value="data.editableCondition"
+                    @input="emit('conditionChange', $event)"
+                    @mousedown.stop
+                    style="width: 100%; height: 150px; margin-top: 8px; resize: none"
+                ></textarea>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  data: any,
+}>()
+
+const emit = defineEmits(['conditionChange']);
+</script>
+
+<style scoped lang="scss">
+
+</style>
