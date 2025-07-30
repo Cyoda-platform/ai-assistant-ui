153abb210c1a10c4899e4f449a8d6f8c951c6db9
alexander.savenko <alexander.savenko@cyoda.com>
2025-07-31 02:09:05 +0300

    Integrate i18n plugin and ensure locale messages are loaded before mounting the app.

--- packages/desktop/src/renderer.ts
+++ packages/desktop/src/renderer.ts
@@ -42,6 +42,7 @@ import router from '@/router';
 // Plugins
 import elementUi from "@/plugins/element-ui.ts";
 import auth0 from "@/plugins/auth0.ts";
+import i18nPlugin, { loadLocaleMessages } from '@/plugins/i18n';
 
 const app = createApp(App);
 
@@ -51,7 +52,9 @@ app.use(createPinia());
 app.use(elementUi);
 app.use(router);
 app.use(auth0);
+app.use(i18nPlugin);
 
 import '@/assets/css/main.scss';
-
-app.mount('#app');
+loadLocaleMessages('en').then(() => {
+    app.mount('#app');
+});
