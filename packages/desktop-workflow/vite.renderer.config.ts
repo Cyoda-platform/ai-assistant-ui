c0adf271296e27761d820dd6209482eed6bd8075
alexander.savenko <alexander.savenko@cyoda.com>
2025-08-31 21:27:03 +0300

    Init

---
+++ packages/desktop-workflow/vite.renderer.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import svgLoader from 'vite-svg-loader';
+
+// https://vitejs.dev/config
+export default defineConfig({
+    plugins: [vue(), svgLoader()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, '../web/src'),
+        },
+    },
+});
